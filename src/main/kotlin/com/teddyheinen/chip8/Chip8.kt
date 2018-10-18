dfd94cae5e81d6e99caf02a06aff1e13e937ab80
Teddy Heinen <skyshayde@gmail.com>
2018-10-18 15:10:46 -0500

    read rom into memory

--- src/main/kotlin/com/teddyheinen/chip8/Chip8.kt
+++ src/main/kotlin/com/teddyheinen/chip8/Chip8.kt
@@ -1,5 +1,19 @@
 package com.teddyheinen.chip8
 
+import java.io.BufferedInputStream
+import java.io.DataInputStream
+import java.io.FileInputStream
+
 fun main(args: Array<String>) {
-    println("Hello World")
+    val state = loadRom("roms/pong.ch8")
+    println(state.ram[0x200])
+}
+
+fun loadRom(rom: String): EmuState {
+    return DataInputStream(BufferedInputStream(FileInputStream(rom))).use {
+        val state = EmuState()
+        val rom = it.readBytes()
+        System.arraycopy(rom,0,state.ram,state.pc,rom.size)
+        state
+    }
 }
